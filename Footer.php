8f91bfb411969416744570fd9197a56133ac852c
Dheemaan Daash <53612989+DheemaanDaash@users.noreply.github.com>
2019-09-27 22:40:30 +0600

    Add files via upload

---
+++ Footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<style>
+	.footer{
+		background: #303036;
+		color: #d3d3d3;
+		height: 400px;
+		position: relative;
+	}
+	.footer .footer-bottom{
+		background: #343a40;
+		color: #686868;
+		height: 50px;
+		width: 100%;
+		text-align: center;
+		position: absolute;
+		bottom: 0px;
+		left:0px;
+		padding-top:20px; 
+	}
+</style>
+
+<div class = "footer">
+	<div class = "footer-content">
+		<div class="footer-section about"></div>
+		<div class="footer-section links"></div>
+		<div class="footer-section contact-form"></div>
+	</div>
+		<div class = "footer-bottom">
+			&copy; www.www.com | Designed by DHIMAN.
+		</div>
+		
+</div>
+
+</html>
